0dadf413f5c9a26232b2d196685f03780750f1c5
Smaragdus7 <smar.leeft@gmail.com>
2022-05-26 10:05:46 -0500

    first comit

---
+++ main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age = 90 - int(age)
+days_left = age * 365
+weeks_left = age * 52
+months_left = age * 12
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+
+
+
